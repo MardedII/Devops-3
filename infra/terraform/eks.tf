940b5d8b56787da313d898a2456b68827a4f822e
Marded <vi.vilchesr@gmail.com>
2026-06-20 14:53:47 -0400

    formato de terraform corregido

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -1,23 +1,23 @@
 #EKS: elastic kubernet service:
 resource "aws_eks_cluster" "eks" {
-  name = "despacho-cluster"
+  name     = "despacho-cluster"
   role_arn = data.aws_iam_role.labrole.arn
   vpc_config {
     subnet_ids = [aws_subnet.eks_subnet_1.id,
-                  aws_subnet.eks_subnet_2.id]
-  }     
+    aws_subnet.eks_subnet_2.id]
+  }
 }
 resource "aws_eks_node_group" "workers" {
-  cluster_name = aws_eks_cluster.eks.name
+  cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "workers"
-  node_role_arn = data.aws_iam_role.labrole.arn
+  node_role_arn   = data.aws_iam_role.labrole.arn
   subnet_ids = [aws_subnet.eks_subnet_1.id,
-                aws_subnet.eks_subnet_2.id]
+  aws_subnet.eks_subnet_2.id]
   scaling_config {
     desired_size = 2
-    max_size = 2
-    min_size = 1
+    max_size     = 2
+    min_size     = 1
   }
   instance_types = ["t3.medium"]
-  capacity_type = "ON_DEMAND"
+  capacity_type  = "ON_DEMAND"
 }
